d504c8cf2352dca3e8c941aa08aa32fab4dcf3ed
gganb <gksql0801@naver.com>
2025-04-09 20:57:51 +0900

    Fix : 사용하지 않는 메서드 임시 주석처리

--- src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.example.newsfeed.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +14,9 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment>  findByBoardIdOrderByUpdatedAtDesc(Long boardId);
 
-    Comment findByBoardId(Long id);
+   // Comment findByBoardId(Long id);
 
     Page<Comment> findAllByBoard_Id(Long id, Pageable pageable);
 
-    // page<Comment> findAllByOrderByUpdatedAtDesc(Pageable pageable);
+
 }
