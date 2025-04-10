0687a34266815caeb4021c58ffdffd53f14ed56d
Shineunju <suj9810@naver.com>
2025-04-10 17:53:57 +0900

    Refactor: 필요없는 import 재 삭제

--- src/main/java/com/example/newsfeed/user/repository/UserRepository.java
+++ src/main/java/com/example/newsfeed/user/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.example.newsfeed.user.repository;
 
 import com.example.newsfeed.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
