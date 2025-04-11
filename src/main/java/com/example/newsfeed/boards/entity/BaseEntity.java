d7d87af7f56fb78218fb3fa1d6baa5e5f2662c9f
idkimjunyng <kimjunyng@icloud.com>
2025-04-11 12:15:16 +0900

    DTO update, 생성 시 날짜 null 업데이트

--- src/main/java/com/example/newsfeed/boards/entity/BaseEntity.java
+++ src/main/java/com/example/newsfeed/boards/entity/BaseEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -14,11 +16,14 @@ import java.time.LocalDateTime;
 @EntityListeners(EntityListeners.class)
 public abstract class BaseEntity {
 
+    @CreationTimestamp
     @CreatedDate
-    @Column(updatable = false)
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
 }
