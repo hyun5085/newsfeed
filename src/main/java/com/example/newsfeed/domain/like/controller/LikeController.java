a111bdb97d5400bf61e60466f48643f75fabe437
gganb <gksql0801@naver.com>
2025-04-12 22:39:19 +0900

    Feat(like) : 좋아요 조회, 삭제 기능 추가, 생성 기능 완성 - 좋아요 생성 : createLike(), 토근값으로 접근 가능하며 댓글id와 함께 서비스로 전달, 응답코드 201 Created - 좋아요 전체 조회 : getLikeInfo(), 토근값으로 접근 가능하며 댓글id와 함께 서비스로 전달, 응답코드 200 Ok - 좋아요 삭제 : deleteLike(), 본인의 좋아요만 삭제할 수 있기에 토큰값을 함께 전달하도록 구성함. 응답코드 200 Ok

--- src/main/java/com/example/newsfeed/domain/like/controller/LikeController.java
+++ src/main/java/com/example/newsfeed/domain/like/controller/LikeController.java
@@ -2,6 +2,7 @@ package com.example.newsfeed.domain.like.controller;
 
 import com.example.newsfeed.common.util.JwtUtil;
 import com.example.newsfeed.domain.like.dto.LikeResponseDto;
+import com.example.newsfeed.domain.like.dto.LikeUpdateResponseDto;
 import com.example.newsfeed.domain.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +16,59 @@ public class LikeController {
     private final LikeService likeService;
     private final JwtUtil jwtUtil;
 
+    /**
+     * 좋아요 생성 API
+     *
+     * @param commentId
+     * @param authorizationHeader
+     * @return
+     */
     @PostMapping
-    public ResponseEntity<LikeResponseDto> createLike(
+    public ResponseEntity<LikeUpdateResponseDto> createLike(
             @PathVariable Long commentId,
             @RequestHeader("Authorization") String authorizationHeader
     ) {
         String token = authorizationHeader.substring(7);
         Long userId = jwtUtil.extractUserId(token);
-        LikeResponseDto likeDto = likeService.createLike(commentId, userId);
-        return ResponseEntity.ok(likeDto);
+
+        LikeUpdateResponseDto likeDto = likeService.createLike(commentId, userId);
+        return ResponseEntity.status(201).body(likeDto);
     }
 
+    /**
+     * 좋아요 전체 조회 API
+     * @param commentId
+     * @param authorizationHeader
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<LikeResponseDto> getLikeInfo(
+            @PathVariable Long commentId,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        String token = authorizationHeader.substring(7);
+        Long userId = jwtUtil.extractUserId(token);
+
+        LikeResponseDto likeAll = likeService.getLikeInfo(commentId, userId);
+        return ResponseEntity.ok(likeAll);
+    }
+
+    /**
+     * 좋아요 삭제 API
+     * 자신의 좋아요만 삭제 가능
+     * @param commentId
+     * @param authorizationHeader
+     * @return
+     */
+    @DeleteMapping
+    public ResponseEntity<Void> deleteLike(
+            @PathVariable Long commentId,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        String token = authorizationHeader.substring(7);
+        Long userId = jwtUtil.extractUserId(token);
+
+        likeService.deleteLike(commentId, userId);
+        return ResponseEntity.ok().build();
+    }
 }
